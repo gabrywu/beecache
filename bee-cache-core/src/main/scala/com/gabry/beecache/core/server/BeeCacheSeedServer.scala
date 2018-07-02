bdd37f7f189100d36eacc3be4ebe1cae01bbe560
gabry <wushaojie@shrbank.com>
2018-07-02 18:06:58 +0800

    seed节点自注册成功

--- bee-cache-core/src/main/scala/com/gabry/beecache/core/server/BeeCacheSeedServer.scala
+++ bee-cache-core/src/main/scala/com/gabry/beecache/core/server/BeeCacheSeedServer.scala
@@ -1,6 +1,9 @@
 package com.gabry.beecache.core.server
 
 import akka.actor.ActorSystem
+import akka.cluster.Cluster
+import com.gabry.beecache.core.Node
+import com.gabry.beecache.core.registry.RegistryFactory
 import com.typesafe.config.ConfigFactory
 
 /**
@@ -8,15 +11,41 @@ import com.typesafe.config.ConfigFactory
   */
 object BeeCacheSeedServer {
   def main(args: Array[String]): Unit = {
+    val defaultConfig = ConfigFactory.load()
+    val registry = RegistryFactory.getRegistry(defaultConfig).getOrElse(null)
+    println(s"registry=$registry")
 
-    val seedConfig = ConfigFactory.load("default/seed.conf")
-    println(s"seecConfig=$seedConfig")
-    val config = ConfigFactory.parseString(s"akka.remote.netty.tcp.port=2551")
-      .withFallback(ConfigFactory.parseString("akka.cluster.roles=[\"seed\"]"))
-      .withFallback(ConfigFactory.load())
+    val seeds = if( registry != null ){
+      try{
+        registry.connect()
+        registry.getNodesByType("seed").map(node=>"\""+node.anchor +"\"")
+      }catch {
+        case exception:Exception=>
+          println(s"registry has errors: $exception")
+          Array.empty[String]
+      }
+    }else
+      Array.empty[String]
+
+    val config = if( seeds.nonEmpty )
+      defaultConfig.getConfig("seed")
+        .withFallback(ConfigFactory.parseString(s"akka.cluster.seed-nodes=[${seeds.mkString(",")}]"))
+        .withFallback(ConfigFactory.load())
+    else
+      defaultConfig.getConfig("seed").withFallback(ConfigFactory.load())
 
     val clusterName = config.getString("clusterNode.cluster-name")
     val system = ActorSystem(clusterName, config)
-
+    val cluster = Cluster(system)
+    cluster.join(cluster.selfAddress)
+    if(registry!=null){
+      println(s"registry node ${Node("seed",cluster.selfAddress.toString)}")
+      registry.registerNode(Node("seed",cluster.selfAddress.toString))
+    }
+    system.registerOnTermination{
+      if(registry!=null){
+        registry.disConnect()
+      }
+    }
   }
 }
